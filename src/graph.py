03a531796cc781bac4eb92b720561be40e42d72d
Jack Umina <jumina@gwu.edu>
2021-02-25 12:20:04 -0500

    Implemented mrv function

--- src/graph.py
+++ src/graph.py
@@ -5,11 +5,11 @@ class Vertex:
         self.colors_remain = colors # All possible remaining colors
         self.adj = set() # Track nodes this one is adjacent to
 
-    def add_neighbor(self, neighbor):
-        self.adj.add(neighbor)
-
     def color(self, color):
         self.color = color
+
+    def add_neighbor(self, neighbor):
+        self.adj.add(neighbor)
 
     def __str__(self):
         return str(self.id) + " : " + str(self.color)
@@ -46,3 +46,39 @@ class Graph:
             s += "\n"
         return s
 
+    def lcv(self, v):
+        return None
+
+    def mrv(self):
+        max_constrn_node = None
+
+        for v in self.nodes:
+            # Get vertex obj of v
+            v = self.nodes[v]
+
+            # If this vertex has already been colored, continue
+            if v.color != None:
+                continue
+
+            if max_constrn_node == None:
+                max_constrn_node = v
+            elif len(v.colors_remain) < len(max_constrn_node.colors_remain):
+                max_constrn_node = v
+            elif len(v.colors_remain) == len(max_constrn_node.colors_remain):
+                if len(v.adj) > len(max_constrn_node.adj):
+                    max_constrn_node = v
+
+        return max_constrn_node
+
+    
+    #def rm_inconsistent_vals(self, v, u):
+    #def ac3(self):
+
+    def color_graph(self):
+        while v := self.mrv():
+            lcv = self.lcv(v)
+            v.color(lcv)
+            self.ac3()
+
+        for v in self.nodes:
+            print(self.nodes[v]) 
